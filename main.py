abe3e54e1a1614ee894cfd782736103d6c652be8
Sujoy Ray <157723180+SUJOY-RAY@users.noreply.github.com>
2024-12-14 22:46:04 +0530

    14 DEC

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from sklearn.model_selection import train_test_split
 
 
 
-
-
 # X, y = make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=42)
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 #
